217ca59de4347303c8a868808db0c2f1bf47ea37
TheGigaFair <k1ngcr0wf0rever@gmail.com>
2025-09-24 22:38:51 -0300

    commit inicial com projeto Unity

---
+++ HelloGit/Assets/Scripts/Hello0.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Hello0 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello GitHub");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
